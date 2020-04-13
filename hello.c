181275adf660cb5900b3d51731d72fd896b04483
DongWoo Kang <kdw941030@naver.com>
2020-04-13 11:10:52 +0900

    Add swap() body

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-extern void swap(int *x, int *y);
+void swap(int *x, int *y)
+{
+	int temp = *x;
+	*x= *y;
+	*y = temp;
+}
 
 int main ()
 {
-	printf("hello world");
+        printf("hello world");
 
 	return 0;
 }
